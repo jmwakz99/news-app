6aa14f91bec06ab16dc5d7c00e1d52b121698b1f
Johnson Mwakazi <mwanjilajohnson@gmail.com>
2020-07-17 12:29:46 +0300

    Updates

---
+++ precache-manifest.d6bb162bed18bae0fda3cc7f2550743a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9af76234135253ca6fc2a4f4e6688825",
+    "url": "/news-app/index.html"
+  },
+  {
+    "revision": "9cc5df59469a60542bcf",
+    "url": "/news-app/static/css/main.5e6abb9a.chunk.css"
+  },
+  {
+    "revision": "df8062be5c10367aebbd",
+    "url": "/news-app/static/js/2.e2de3377.chunk.js"
+  },
+  {
+    "revision": "228bac5b7587832345b091357f4a52ce",
+    "url": "/news-app/static/js/2.e2de3377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cc5df59469a60542bcf",
+    "url": "/news-app/static/js/main.8dea13ae.chunk.js"
+  },
+  {
+    "revision": "b030bcf33a5f34fd332f",
+    "url": "/news-app/static/js/runtime-main.4504e9de.js"
+  }
+]);
